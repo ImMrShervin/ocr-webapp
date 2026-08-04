3054c5c0c972b95474924cbbb3110940b3d74c43
Shervin Mirzaei <shervinmr84@gmail.com>
2026-08-04 19:47:20 +0330

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ from werkzeug.utils import secure_filename
  
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-DetectorFactory.seed = 0 
+
+
+DetectorFactory.seed = 1
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
